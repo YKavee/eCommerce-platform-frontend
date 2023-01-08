7a88cf0c882dfa00c230e278e7f9e59b126c618c
YKavee <ykaveesenanayake@gmail.com>
2023-01-08 16:39:22 +0530

    Added jest dependecies

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,6 @@ module.exports = {
       "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|scss)$": "identity-obj-proxy",
   },
-  setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
 };
